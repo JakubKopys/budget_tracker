8ab56f9490afc81dbbce0e4aa778d62d39d05f9d
Jakub <jakub.kopys@tlen.pl>
2018-06-02 22:55:33 +0200

    Invites form specs

---
+++ spec/forms/invites/invite_form_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'support/shared_examples/application_form'
+
+RSpec.describe Invites::InviteForm, type: :model do
+  it_behaves_like 'application_form'
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :user }
+    it { is_expected.to validate_presence_of :household }
+
+    it 'validates household residents exclusion' do
+      user = create :user
+      household = create :household
+      household.users << user
+
+      form = described_class.new(user: user, household: household)
+
+      expect(form.valid?).to be false
+      errors = form.errors.details[:user]
+      expect(errors.first[:error]).to eq 'is already an household resident'
+    end
+  end
+end
--- spec/forms/users/register_form_spec.rb
+++ spec/forms/users/register_form_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe Users::RegisterForm, type: :model do
   it_behaves_like 'basic_form'
 
   describe 'validations' do
-    it { is_expected. to validate_presence_of :email }
-    it { is_expected. to validate_presence_of :first_name }
-    it { is_expected. to validate_presence_of :last_name }
-    it { is_expected. to validate_presence_of :password }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :password }
   end
 end
